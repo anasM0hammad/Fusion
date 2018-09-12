925d77a6feab63b0de95f34f5e7385c89ae71bb2
Mohammad Anas <anas.1633.m@gmail.com>
2018-09-12 23:18:15 +0530

    Starting project

---
+++ index.php
@@ -0,0 +1,22 @@
+
+<!-- WELCOME MOHAMMAD ANAS -->
+
+<!-- Lets code -->
+
+<!doctype html>
+<html>
+<head>
+    
+
+     <meta charset="utf-8" />
+     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+     <meta name="viewport" content="width=device-width, initial-scale=1" />
+     
+      <title></title>
+        
+</head>
+
+<body>
+
+</body>
+</html>
