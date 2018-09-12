d515a6d4b0b3543b371f55b5415e261fdc6cc3e3
Mohammad Anas <anas.1633.m@gmail.com>
2018-09-13 01:24:54 +0530

    header done

--- index.php
+++ index.php
@@ -4,19 +4,67 @@
 <!-- Lets code -->
 
 <!doctype html>
-<html>
-<head>
-    
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-     <meta charset="utf-8" />
-     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-     <meta name="viewport" content="width=device-width, initial-scale=1" />
-     
-      <title></title>
-        
-</head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      
+      <!--Font Awesome -->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 
-<body>
+    <link href="img/icon.png" rel="icon" type ="image/gif" sizes="any"> 
+      
+    <link rel="stylesheet" type="text/css" href="css/home.css">  
+      
+    <title>Fusion</title>
+  </head>
+  <body>
+      
+      
+      <!-- NAVBAR  -->  
+      
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><img src="img/icon.png" width="30" height="30" class="d-inline-block align-top" alt="">
+    Fusion</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-</body>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav container justify-content-end list">
+      <li class="nav-item active">
+          <a class="nav-link" href="#"><i class ="fas fa-home"></i> Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fas fa-globe"></i> About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      
+      <!-- NAVBAR ENDS HERE -->
+      
+      <!--MAIN PAGE HEADER-->
+      
+      <div class="header">
+      
+        <img src="img/icon2.png" class="rounded mx-auto d-block">
+        <h2>Welcome to Fusion</h2>
+          <p>Fusion is a combination of a blog and a social platform <br><span class = "d-none d-sm-block"> for Developers, Technology enthusiast, students and anyone who loves technology and Science.</span></p>  
+      
+      
+      </div>
+      
+   
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
 </html>
