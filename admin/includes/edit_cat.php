b80ff89cb17ba0913ef1648db173bb2d00938001
Mohammad Anas <anas.1633.m@gmail.com>
2018-09-28 11:31:52 +0530

    Updating category done

--- admin/includes/edit_cat.php
+++ admin/includes/edit_cat.php
@@ -1,7 +1,24 @@
-  
+              <?php
+                      //UPDATING
+                       if(isset($_POST['update'])){
+                            $upd_cat = $_POST['cat_title'] ;
+                            $upd_query = "UPDATE category SET cat_title = '{$upd_cat}' WHERE cat_id = {$upd_cat_id}" ;
+                            $upd_query_result = mysqli_query($connect , $upd_query) ;
+                            
+                            if(!$upd_query_result){
+                                die("QUERY FAILED").mysqli_error($connect);
+                            }
+                
+                         else{
+                          echo "<div class='alert alert-success' style='margin-bootom:20px; border-radius:0;' role='alert'><b>Category Updated</b></div>";
+                          
+                      }
+                    }
+                ?>
+
                 <h5 class="mb-2"><b>Edit Category</b></h5>
                 
-                <form action="category.php" method="post" class="form-inline">
+                <form action="" method="post" class="form-inline">
                   <div class="form-group ">
                       
                       <?php 
@@ -20,24 +37,9 @@
                       <input type="text" class="form-control" placeholder="Category" name="cat_title" value="<?php echo $cat_title ; ?>">
                         
                                
-                          <?php } //while ?>
+                          <?php } //while
                       
-                      <?php 
-                     
-                  }
-                      //UPDATING
-                       if(isset($_POST['update'])){
-                            $upd_cat = $_POST['cat_title'] ;
-                            $upd_query = "UPDATE category SET cat_title = '{$upd_cat}' WHERE cat_id = {$upd_cat_id}" ;
-                            $upd_query_result = mysqli_query($connect , $upd_query) ;
-                            
-                            if(!$upd_query_result){
-                                die("QUERY FAILED").mysqli_error($connect);
-                            }
-                   
-                
-                      ?>
-                     <?php }// if ?>
+                       } ?>
                       
                   </div>
                   <button type="submit" class="btn btn-primary" style="margin-left:5px; border-radius:0;" name="update">Update</button>
