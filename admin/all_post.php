0732097ef18208941a9bb8a0202472140578c643
anasM0hammad <anas.1633.m@gmail.com>
2018-09-28 22:22:26 +0530

    post table (admin)

--- admin/all_post.php
+++ admin/all_post.php
@@ -1,7 +1,5 @@
   <!-- HEADER -->
    <?php include "includes/header.php" ; ?>
-      
-      
 
 
   <!-- NAVBAR -->
@@ -26,7 +24,7 @@
               <li class="nav-item">
                 <a class="nav-link" href="category.php"><i class="fas fa-chart-pie"></i> Categories</a>
               </li>
-              <li class="nav-item dropdown">
+              <li class="nav-item dropdown active">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="far fa-clipboard"></i> Posts</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <a class="dropdown-item" href="#">Add Post</a>
@@ -56,11 +54,41 @@
         <!-- MAIN CONTENT -->
          <div class="col-md-10">
              <h2 class="heading"><b>Welcome to Admin Page</b></h2><hr><br>   
+             
+             <!-- TABLE -->
+             <table class="table table-striped table-hover table-bordered">
+              <thead>
+                 <tr>
+                   <th>Id</th>
+                   <th>Author</th>
+                   <th>Title</th>
+                   <th>Category</th>
+                   <th>Status</th> 
+                   <th>Image</th>
+                   <th>Tags</th>
+                   <th>Comment</th>
+                   <th>Date</th>     
+                 </tr>
+              </thead>
+              <tbody>
+                 <tr>
+                   <td>1</td>
+                   <td>Mohammad Anas</td>
+                   <td>How to start Web Development</td>
+                   <td>Web Development</td>
+                   <td>Yes</td>
+                   <td>image</td> 
+                   <td>Tags,tags</td>
+                   <td>comment</td>
+                   <td>12/3/4/2018</td>      
+                 </tr>
+             </tbody>     
+             </table>
             
         </div>    
             
             
-        </div>
+        </div> <!-- ROW END -->
       
       
       <!-- FOOTER -->
--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,4 +1,12 @@
 
+      <div class="footer">
+       <div class = "container">
+          <h4><img src="img/icon2.png" height="50" width="50"> Fusion</h4>
+          <p>All Rights Reserved <i class="far fa-copyright"></i> 2018</p>   
+       </div>     
+      </div>
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
