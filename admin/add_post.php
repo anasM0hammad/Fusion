e5149947f881d344f34e3ef58bc70a1470169f37
anasM0hammad <anas.1633.m@gmail.com>
2018-09-29 23:38:53 +0530

    publishing post done post (admin)

--- admin/add_post.php
+++ admin/add_post.php
@@ -55,48 +55,89 @@
          <div class="col-md-10">
              <h2 class="heading"><b><i class="fas fa-database"></i> Enter Post Details</b></h2><hr><br>   
             
+             <?php 
+              if(isset($_POST['publish'])){
+                  
+                  $post_title = $_POST['title'];
+                  $post_author = $_POST['author'];
+                  $post_tags = $_POST['tags'];
+                  $post_status = $_POST['status'] ;
+                  $post_cat = $_POST['category'];
+                  $post_content = $_POST['content'];
+                  $post_image = $_FILES['image']['name'];
+                  $post_image_tmp = $_FILES['image']['tmp_name'] ;
+                  $post_author_image = $_FILES['auth_image']['name'];
+                  $post_author_image_tmp = $_FILES['auth_image']['tmp_name'];
+                  $post_date = date('d-m-y');
+                  $post_comment_count = 1 ;
+                  
+                  move_uploaded_file($post_image_tmp , "../img/$post_image") ;
+                  move_uploaded_file($post_author_image_tmp , "../img/$post_author_image") ;
+                  
+                  if($post_title=="" || $post_author=="" || $post_content=="" || empty($post_title) || empty($post_author) || empty($post_content)){
+                  echo "<div class='alert alert-danger' style='margin-bootom:20px; border-radius:0;' role='alert'><b>Enter Required Fields..!</b></div>";
+                                 
+                  }
+                  
+                  else{
+                  
+                  $publish_query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_comment_count, post_content,     post_tags, post_status, post_author_image) VALUES ({$post_cat}, '{$post_title}', '{$post_author}', now(), '{$post_image}', {$post_comment_count}, '{$post_content}', '{$post_tags}', '{$post_status}', '{$post_author_image}') ";
+                  
+                  $publish_result = mysqli_query($connect, $publish_query) ;
+                  
+                  echo "<div class='alert alert-success' style='margin-bootom:20px; border-radius:0;' role='alert'><b>Post Published</b></div>";
+                      
+                  }
+              }
+             
+             ?>
              
              <!-- FORM -->
-             <form class="container">
+         <form class="container" action="" method="post" enctype="multipart/form-data">
           <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="title"><b>Title</b></label>
-              <input type="text" class="form-control" placeholder="Title">
+              <input type="text" class="form-control" placeholder="Title" name="title">
             </div>
             <div class="form-group col-md-6">
                 <label for="author"><b>Author Name</b></label>
-              <input type="text" class="form-control" placeholder="Author">
+              <input type="text" class="form-control" placeholder="Author" name="author">
             </div>
           </div>
                  
           <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="tag"><b>Post Tags</b></label>
-              <input type="text" class="form-control" placeholder="Tags">
+              <input type="text" class="form-control" placeholder="Tags" name="tags">
             </div>
             <div class="form-group col-md-6">
                 <label for="status"><b>Post Status</b></label>
-              <input type="text" class="form-control" placeholder="status">
+              <input type="text" class="form-control" placeholder="status" name="status">
             </div>
           </div>
+              
+             <div class="form-group ">
+                <label for="category"><b>Category</b></label>
+              <input type="text" class="form-control" name="category">
+            </div>
                  
           <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="category"><b>Category</b></label>
-              <input type="text" class="form-control">
-            </div>
+            <div class="form-group">
+                  <label for="image"><b>Author Image</b></label>
+                <input type="file" class="form-control-file" name="auth_image" >
+              </div>
             <div class="form-group col-md-6">
               <div class="form-group">
                   <label for="image"><b>Image</b></label>
-                <input type="file" class="form-control-file" >
+                <input type="file" class="form-control-file" name="image" >
               </div>
             </div>
           </div>
           <div class="form-group">
             <label for="content"><b>Content</b></label>
-            <textarea class="form-control" rows="4"></textarea>
+            <textarea class="form-control" rows="4" name="content"></textarea>
           </div>         
-          <button type="publish" class="btn btn-primary" style="border-radius:0;">Publish</button>
+          <button type="publish" class="btn btn-primary d-block mx-auto btn-block" style="border-radius:0;" name="publish">Publish</button>
         </form>
            
         </div>    
