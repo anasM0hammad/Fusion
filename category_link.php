72f8c007738e3e934490fa851905f9cd37d56e3b
anasM0hammad <anas.1633.m@gmail.com>
2018-10-13 10:25:00 +0530

    last commit updated

--- category_link.php
+++ category_link.php
@@ -15,6 +15,29 @@
   <body>
       
       
+       <?php 
+      //QUERY TO SHOW INFO OF USER IN PROFILE BAR
+      
+      if(isset($_SESSION['username'])){
+        $username =  $_SESSION['username'] ;
+          
+          $prof_query = "SELECT * FROM users WHERE username = '$username' " ;
+          $prof_result = mysqli_query($connect, $prof_query);
+          
+          while($row = mysqli_fetch_assoc($prof_result)){
+              $firstname = $row['user_firstname'];
+              $lastname = $row['user_lastname'];
+              $image = $row['user_image'];
+              $role = $row['user_role'];
+          }
+          
+      }
+      
+      
+      ?>
+      
+      
+      
       <!-- NAVBAR  -->  
       
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -25,19 +48,54 @@
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    
+    <!-- PROFILE BAR -->  
+    <?php if(isset($_SESSION['username'])){ ?>  
+    <ul  class="navbar-nav container justify-content-end " >
+     <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><i class="far fa-user"></i> <?php echo $firstname . " " . $lastname . " "; ?></a>
+        <div class="dropdown-menu drop-link">
+          <a class="dropdown-item" href="profile.php"><i class="far fa-user"></i> Profile</a>
+          <a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Settings</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="includes/logout.php"><i class="fas fa-power-off"></i> Log Out</a>
+        </div>
+      </li> 
+     </ul>  
+      <?php } ?>
+      
     <ul class="navbar-nav container justify-content-end list">
       <li class="nav-item active">
           <a class="nav-link" href="index.php"><i class ="fas fa-home"></i> Home<span class="sr-only">(current)</span></a>
       </li>
+        
+        
+        <?php if(isset($_SESSION['username'])){ 
+              if($role == 'Admin'){   
+          
+            echo  "<li class='nav-item'>
+                <a class='nav-link' href='admin/index.php'><i class='fas fa-globe'></i> Admin</a>
+              </li>";  } 
+        } ?>
+        
+        
+        <?php if(!isset($_SESSION['username'])){ ?>
       <li class="nav-item">
-        <a class="nav-link" href="admin/index.php"><i class="fas fa-globe"></i> Admin</a>
+        <a class="nav-link" href="registration.php"><i class="fas fa-user-plus"></i> Register</a>
       </li>
+        <?php }?>
+        
+        
        <li class="nav-item">
         <a class="nav-link" href="#"><i class="fas fa-phone"></i> Contact</a>
-      </li>    
+      </li>
+
     </ul>
+      
   </div>
 </nav>
+      
+
       
       <!-- NAVBAR ENDS HERE -->
       
